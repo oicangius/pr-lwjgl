72910e9c64f665c8ebc2c8d433b4c54fbc7a53d0
oican <oicangius@gmail.com>
2012-10-31 12:23:44 +0100

    Game añadido

---
+++ pr-lwjgl/src/es/uca/ignacio_garcia/Game.java
@@ -0,0 +1,13 @@
+package es.uca.ignacio_garcia;
+
+public class Game {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hola");
+	}
+
+}
